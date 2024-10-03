aed8382c4519488bb90f6fff788089d6de162860
Elle <56184436864@info.local>
2024-10-03 16:44:09 -0300

    bibliotec

---
+++ bibliotec-DDL.sql
@@ -0,0 +1,36 @@
+
+USE Bibliotec;
+
+
+DROP TABLE Usuario;
+DROP TABLE Curso;
+
+
+CREATE TABLE Usuario(
+	UsuarioID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	Nome VARCHAR(100),
+	DtNascimento DATE,
+	Email VARCHAR(100),
+	Senha VARCHAR(20),
+	Contato CHAR(14),
+	Admin BIT,
+	Status BIT
+);
+
+ALTER TABLE Usuario
+ALTER COLUMN UsuarioID INT NOT NULL;
+
+ALTER TABLE Usuario
+ADD PRIMARY KEY (UsuarioID);
+
+CREATE TABLE Curso(
+	CursoID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	Nome VARCHAR(100),
+	Periodo CHAR(1)
+);
+
+ALTER TABLE Curso
+ALTER COLUMN CursoID INT NOT NULL;
+
+ALTER TABLE Curso
+ADD PRIMARY KEY (CursoID);
